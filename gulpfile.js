630784f3cc9f29a39d32c2797113769081fa665b
Tyler Vick <tylerjvick@gmail.com>
2018-04-07 22:38:17 -0700

    Critical css generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
+const critical = require('critical').stream;
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
@@ -15,6 +16,18 @@ gulp.task('css', () => {
         .pipe(gulp.dest('./css'));
 });
 
+gulp.task('critical', () => {
+   return gulp.src('./index.html')
+       .pipe(critical({
+           base: process.cwd(),
+           inline: false,
+           css: ['css/main.css', 'css/normalize.css', 'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css'],
+           minify: true,
+       }))
+       .pipe(rename('critical.min.css'))
+       .pipe(gulp.dest('./css'));
+});
+
 gulp.task('js', () => {
     return gulp.src(jsPath)
         .pipe(babel({
@@ -27,5 +40,6 @@ gulp.task('js', () => {
 
 gulp.task('default', () => {
     gulp.watch(cssPath, ['css']);
+    gulp.watch('./index.template.html', ['critical']);
     gulp.watch(jsPath, ['js']);
 });
